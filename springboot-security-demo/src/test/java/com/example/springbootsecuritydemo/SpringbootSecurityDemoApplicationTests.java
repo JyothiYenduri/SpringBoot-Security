d0c93a0dab51161a183ccf37cb4547c65983cefc
JyothiYenduri <120366268+JyothiYenduri@users.noreply.github.com>
2023-09-30 11:19:46 +0530

    Add files via upload

---
+++ springboot-security-demo/src/test/java/com/example/springbootsecuritydemo/SpringbootSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
